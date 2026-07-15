a2f9aa44f825ec53e1094ae46034da7cf9ecab2b
RatebAK <sa.20.sy@gmail.com>
2026-07-15 16:38:19 +0300

    docs: compiled scribe assets with production domain fix

--- config/scribe.php
+++ config/scribe.php
@@ -28,7 +28,8 @@ return [
     // If you're using `laravel` type, you can set this to a dynamic string, like '{{ config("app.tenant_url") }}' to get a dynamic base URL.
     //'base_url' => config('app.url'),
 
-    'base_url' => 'https://laravel.cloud',
+    'base_url' => 'https://joboffers-main-uj2ehj.free.laravel.cloud',
+
 
     // Routes to include in the docs
     'routes' => [
